34d3754d30050137347a422b7f8b7874482513c6
myml <wurongjie1@gmail.com>
2019-08-17 18:55:11 +0800

    feat: add json tag

--- parser.go
+++ parser.go
@@ -10,21 +10,22 @@ import (
 
 // Package control
 type Package struct {
-	Package       string
-	Status        string
-	Priority      string
-	Architecture  string
-	MultiArch     string
-	Maintainer    string
-	Version       string
-	Section       string
-	InstalledSize int64
-	Depends       string
-	PreDepends    string
-	Description   string
-	Source        string
-	Homepage      string
-	Raw           map[string]string
+	Package       string `json:"package"`
+	Status        string `json:"status"`
+	Priority      string `json:"priority"`
+	Architecture  string `json:"architecture"`
+	MultiArch     string `json:"multi_arch"`
+	Maintainer    string `json:"maintainer"`
+	Version       string `json:"version"`
+	Section       string `json:"section"`
+	InstalledSize int64  `json:"installed_size"`
+	Depends       string `json:"depends"`
+	PreDepends    string `json:"pre_depends"`
+	Description   string `json:"description"`
+	Source        string `json:"source"`
+	Homepage      string `json:"homepage"`
+
+	Raw map[string]string `json:"raw"`
 }
 
 // FromMap convert map to package
